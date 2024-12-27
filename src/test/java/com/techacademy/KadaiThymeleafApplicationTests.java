9f1d6424c7ed689d51d0ee9b30d511613bdcb1d1
zato0627 <panmimi38@icloud.com>
2024-12-28 01:26:38 +0900

    Lesson15課題

---
+++ src/test/java/com/techacademy/KadaiThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KadaiThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
